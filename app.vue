eca5cbee455b074e1c61b067ed282c2e4bb02d06
Gustavo Rodrigues <gustardn@gmail.com>
2025-02-24 21:31:27 -0300

    remade everything with NuxtJS. Vue is better. Fuck it

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Remade everything with NuxtJS, my bad...</h1>
+</template>
+
+<style>
+* {
+  margin: 0;
+}
+
+h1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-weight: 300;
+  font-size: 50px;
+}
+</style>
